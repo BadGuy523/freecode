6e1c121d563547920d854971b11dc950e6f6eb34
BadGuy523 <34163407+BadGuy523@users.noreply.github.com>
2021-11-30 09:34:23 +0800

    Create CustomCellStyleHandler.java

---
+++ common/src/main/java/com/zjq/freecode/common/handler/CustomCellStyleHandler.java
@@ -0,0 +1,29 @@
+/**
+ * @Description: 自定义写excel头部样式
+ * @Author: zhangjunqiang zwx925931
+ * @Date: 2021/11/18
+ */
+public class CustomCellStyleHandler extends AbstractCellStyleStrategy {
+    private Workbook workbook;
+
+    @Override
+    protected void initCellStyle(Workbook book) {
+        this.workbook = book;
+    }
+
+    @Override
+    protected void setHeadCellStyle(Cell cell, Head head, Integer integer) {
+        WriteCellStyle writeCellStyle = new WriteCellStyle();
+        writeCellStyle.setFillForegroundColor(IndexedColors.RED.getIndex());
+        WriteFont font = new WriteFont();
+        font.setFontHeightInPoints(NumberConstants.SHORT_11);
+        writeCellStyle.setWriteFont(font);
+        CellStyle style = StyleUtil.buildHeadCellStyle(workbook, writeCellStyle);
+        cell.setCellStyle(style);
+    }
+
+    @Override
+    protected void setContentCellStyle(Cell cell, Head head, Integer integer) {
+        // do nothing
+    }
+}
