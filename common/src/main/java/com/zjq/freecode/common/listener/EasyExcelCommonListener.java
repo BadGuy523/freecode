35d36e0812f1d6b8717513818d873b346e0cc22b
BadGuy523 <34163407+BadGuy523@users.noreply.github.com>
2021-11-30 09:30:44 +0800

    Update EasyExcelCommonListener.java

--- common/src/main/java/com/zjq/freecode/common/listener/EasyExcelCommonListener.java
+++ common/src/main/java/com/zjq/freecode/common/listener/EasyExcelCommonListener.java
@@ -13,25 +13,113 @@ import java.util.List;
   * @version v1.0
   */
 public class EasyExcelCommonListener <T> extends AnalysisEventListener<T> {
-
-    private List<T> list = new ArrayList<>();
+/**
+     * 数据类型
+     */
+    protected Class tClass;
 
     /**
-     * @Description: 对与每一条数据的处理
-     * @author zhangjunqiang
-     * @param data 一行数据转换成的对象
-     * @date 2021/3/7 19:17
+     * 是否成功
+     */
+    protected Boolean isSuccess = true;
+
+    /**
+     * 错误信息集合
+     */
+    protected List<String> message = new ArrayList<>();
+
+    /**
+     * 数据集合
+     */
+    protected List<T> datas = new ArrayList<>();
+
+    public EasyExcelCommonListener() {
+    }
+
+    public EasyExcelCommonListener(Class clazz) {
+        this.tClass = clazz;
+    }
+
+    /**
+     * 对每一条数据的处理
+     *
+     * @param data 每一条数据object
+     * @param context 上下文
      */
     @Override
     public void invoke(T data, AnalysisContext context) {
-        list.add(data);
+        if (isSuccess) {
+            datas.add(data);
+        }
     }
 
     @Override
     public void doAfterAllAnalysed(AnalysisContext analysisContext) {
+        // do nothing
     }
 
-    public List<T> getList() {
-        return list;
+    /**
+     * 校验表头
+     *
+     * @param headMap 表头map
+     * @param context 上下文
+     */
+    @Override
+    public void invokeHeadMap(Map<Integer, String> headMap, AnalysisContext context) {
+        int row = context.readRowHolder().getRowIndex() + 1;
+        super.invokeHeadMap(headMap, context);
+        if (tClass == null) {
+            return;
+        }
+        List<String> allTitles = new ArrayList<>();
+        List<Field> allFields = new ArrayList<>();
+        addAllFields(tClass, allFields);
+        allFields.forEach(field -> {
+            ExcelProperty declaredAnnotation = field.getDeclaredAnnotation(ExcelProperty.class);
+            if (declaredAnnotation != null) {
+                String[] value = declaredAnnotation.value();
+                allTitles.add(value[row - 1]);
+            }
+        });
+        List<String> actualTitles = new ArrayList<>();
+        for (int i = 0; i < headMap.size(); i++) {
+            actualTitles.add(headMap.get(i));
+        }
+        for (int i = 0; i < allTitles.size(); i++) {
+            if (i > actualTitles.size() - 1) {
+                message.add("第" + row + "行，第" + (i + 1) + "列表头（" + allTitles.get(i) + "）缺失");
+                isSuccess = false;
+            } else if (!StringUtils.equals(actualTitles.get(i), allTitles.get(i))) {
+                message.add("第" + row + "行，第" + (i + 1) + "列表头（" + actualTitles.get(i) + "）错误，应为（" + allTitles.get(i)
+                        + "）");
+                isSuccess = false;
+            }
+        }
+    }
+
+    private void addAllFields(Class clazz, List<Field> fieldList) {
+        if (clazz != null) {
+            fieldList.addAll(Arrays.stream(clazz.getDeclaredFields()).collect(Collectors.toList()));
+            addAllFields(clazz.getSuperclass(), fieldList);
+        } else {
+            return;
+        }
+    }
+
+    public List<T> getDatas() {
+        return datas;
+    }
+
+    public Boolean getSuccess() {
+        return isSuccess;
+    }
+
+    public List<String> getMessage() {
+        return message;
+    }
+
+    public EasyExcelCommonListener<T> settClass(Class tClass) {
+        this.tClass = tClass;
+        return this;
     }
 }
